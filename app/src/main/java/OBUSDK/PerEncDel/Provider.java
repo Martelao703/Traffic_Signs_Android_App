f04d68aaa80aa062e8a157fa58ac9aae8913cb4c
Martelao703 <alves.david.0303@gmail.com>
2024-04-25 19:36:53 +0100

    IVIM rabbit hole done

---
+++ app/src/main/java/OBUSDK/PerEncDel/Provider.java
@@ -0,0 +1,22 @@
+package OBUSDK.PerEncDel;
+
+public class Provider {
+    private A5 countryCode;
+    private int providerIdentifier;
+
+    public A5 getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(A5 countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public int getProviderIdentifier() {
+        return providerIdentifier;
+    }
+
+    public void setProviderIdentifier(int providerIdentifier) {
+        this.providerIdentifier = providerIdentifier;
+    }
+}
