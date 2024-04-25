8a25564dddb079bcc0259cc1dad4adcb2d813654
Martelao703 <alves.david.0303@gmail.com>
2024-04-25 03:53:07 +0100

    s

--- app/src/main/java/OBUSDK/JsonController/JsonAdapter.java
+++ app/src/main/java/OBUSDK/JsonController/JsonAdapter.java
@@ -1,9 +1,14 @@
 package OBUSDK.JsonController;
 
 import OBUSDK.IControllerAdapter;
+import OBUSDK.PerEncDel.IVIM;
+import OBUSDK.IVIMMemoryStructures;
 
 public class JsonAdapter implements IControllerAdapter {
-
     private IVIM rootIVI;
 
+    @Override
+    public IVIMMemoryStructures buildIVIMStructures() {
+        return null;
+    }
 }
---
+++ app/src/main/java/OBUSDK/PerEncDel/IviStructure.java
@@ -0,0 +1,22 @@
+package OBUSDK.PerEncDel;
+
+public class IviStructure {
+    private IviManagementContainer mandatory;
+    private IviContainers optional;
+
+    public IviManagementContainer getMandatory() {
+        return mandatory;
+    }
+
+    public void setMandatory(IviManagementContainer mandatory) {
+        this.mandatory = mandatory;
+    }
+
+    public IviContainers getOptional() {
+        return optional;
+    }
+
+    public void setOptional(IviContainers optional) {
+        this.optional = optional;
+    }
+}
